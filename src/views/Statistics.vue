141ee3f1ff9c36b408b90d4f110c90ee9714beb0
CarolSum <905965287@qq.com>
2018-06-28 13:17:30 +0800

    实现排行榜分页

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -11,19 +11,18 @@
           :data="tableData" 
           style="width: 100%"
           highlight-current-row>
-          <el-table-column type="index" min-width="50"></el-table-column>
+          <el-table-column property="index" label="排行" min-width="50"></el-table-column>
           <el-table-column property="name" label="图书" min-width="200"></el-table-column>
           <el-table-column property="price" label="售价" min-width="120"></el-table-column>
           <el-table-column property="salesVolumn" label="销量" min-width="120"></el-table-column>
         </el-table>
         <el-pagination
           background
-          :page-sizes="[5, 10, 15, 20]"
           :page-size="10"
-          layout="total, sizes, prev, pager, next, jumper"
+          layout="total, prev, pager, next, jumper"
           :total="tableLength"
-          @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
+          :current-page.sync="currentPage"
           style="float: right; margin: 20px 0;">
         </el-pagination>
       </div>
@@ -39,17 +38,22 @@ export default {
       totalSales: 30009,
       totalAmount: 2000,
       tableData: rankListData,
-      tableLength: rankListData.length
+      tableLength: rankListData.length,
+      currentPage: 1
     }
   },
   methods: {
-    handleSizeChange (val) {
-      // 更新表格每页数据
-
-    },
-    handleCurrentChange () {
+    handleCurrentChange (val) {
       // 更新表格内容
+      this.tableData = this.pagination(val, 10, rankListData).map((row, id) => { row.index = id + (this.currentPage - 1) * 10 + 1; return row;});
+    },
+    pagination (pageNo, pageSize, array) {
+      var offset = (pageNo - 1) * pageSize;
+      return (offset + pageSize >= array.length) ? array.slice(offset, array.length) : array.slice(offset, offset + pageSize);
     }
+  },
+  mounted () {
+    this.tableData = this.pagination(1, 10, rankListData).map((row, id) => { row.index = id + (this.currentPage - 1) * 10 + 1; return row;});
   }
 }    
 </script>
